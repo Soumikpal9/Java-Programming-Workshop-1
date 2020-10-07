dc8fdc43ff8dcf0989f4ea954fe1058bd453eab6
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-07 09:24:16 +0530

    [SP]Added: Tic Tac Toe Board Created

--- src/com/workshop/TicTacToe.java
+++ src/com/workshop/TicTacToe.java
@@ -2,14 +2,17 @@ package com.workshop;
 
 public class TicTacToe {
 	
-	public static void createBoard() {
+	public static char[] createBoard() {
 		char[] board = new char[10];
 		for(int i = 1; i < board.length; i++) {
-			i = '0';
+			board[i] = '0';
 		}
+		return board;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game!!!");
+		
+		char[] board = createBoard();
 	}
 }
